40efef717ce7ec02ff0bc5918db14f7e0c1d5e68
Camille Loue <camille.loue28@gmail.com>
2023-08-27 21:11:56 +0100

    added: new pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,26 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="text-center">
-        <h1 className="text-4xl font-semibold mb-4">Camille</h1>
-        <p className="text-lg text-gray-500">Freelance Machine Learning Engineer</p>
-      </div>
-      <div className="my-8">
+    <main className="flex flex-col items-start px-10 lg:px-0">
+      <div className="flex gap-x-6">
         <Image
           src="/images/profile.jpg"
           alt="Your Profile Picture"
-          className="rounded-full"
-          width={150}
-          height={150}
+          className="rounded-full w-20 h-20"
+          width={80}
+          height={80}
         />
+        <div className="flex flex-col gap-y-4">
+          <h1 className="text-4xl font-semibold ">Camille</h1>
+          <p className="text-lg text-gray-500">
+            Freelance Machine Learning Engineer
+          </p>
+        </div>
       </div>
-      <p className="text-center text-gray-600 max-w-md">
+      <p className=" text-gray-600 max-w-md mt-8">
         I am a passionate about Machine Learning and building products online.
       </p>
     </main>
-  )
+  );
 }
-
