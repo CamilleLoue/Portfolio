f7424176fdd3eec269860e5ba3b8c5507a9fd5b1
Camille Loue <camille.loue28@gmail.com>
2023-12-04 23:08:16 +0000

    chore: organisation of work and main pages

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,33 +1,30 @@
 import Image from "next/image";
-import Card from "./components/Card.jsx";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-start px-10 lg:px-0">
       <div className="flex gap-x-6">
         <Image
-          src="/images/profile-ucl.jpg"
+          src="/images/profile.png"
           alt="Profile Picture"
-          className="rounded-full w-22 h-22"
-          width={160}
-          height={160}
+          className="rounded-full w-22 h-22 aspect-square"
+          width={120}
+          height={120}
         />
         <div className="flex flex-col gap-y-2">
-          <h1 className="mt-7 text-3xl font-semibold">Hi, I&apos;m Camille</h1>
+          <h1 className="mt-5 text-2xl text-neutral-200">Hi, I&apos;m Camille</h1>
           <div>
-            <p className="mt-4 text-gray-400 text-lg">
-              Web App enthusiast with a background in Machine Learning.
+            <p className="mt-2 text-neutral-400">
+              I launch ML models and web apps.
             </p>
           </div>
         </div>
       </div>
-      <div className="text-gray-400 text-xl mt-10">Introduction</div>
-      <div className="text-gray-400 text-xl mt-10">Work</div>
-      <Card 
-      title="Real Estate Data Analysis" 
-      dates="Aug 2023 - Sept 2023" 
-      badges={['Data wrangling', 'Data viz']}/>
-      <div className="text-gray-400 text-xl mt-10">Testimonials</div>
+      <div className="text-neutral-400 mt-10">About Me</div>
+      <div className="text-neutral-400 mt-2">Skills and expertise</div>
+      <div className="text-neutral-400 mt-2">Work</div>
+      <div className="text-neutral-400 mt-2">Testimonials</div>
+      <div className="text-neutral-400 mt-2">Projects</div>
     </main>
   );
 }
