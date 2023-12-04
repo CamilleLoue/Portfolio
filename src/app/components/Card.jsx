736c6b8e03c8e1c90255756b04ae5b213f540799
Camille Loue <camille.loue28@gmail.com>
2023-12-04 23:03:16 +0000

    build work page

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
 
-const Card = ({ title, dates, badges }) => {
+const Card = ({ title, dates, skills }) => {
   return (
-    <div className="border border-neutral-200 dark:border-neutral-800 bg-neutral-50 dark:bg-neutral-900 rounded flex flex-col justify-between mt-6 px-3 py-3 w-full">
+    <div className="border font-sans border-neutral-800 bg-neutral-900 rounded flex flex-col justify-between mb-5 px-3 py-3 w-full cursor-pointer hover:card-hover">
       <div className="flex justify-between items-center">
         <div>
-          <h3 className="text-lg font-semibold">{title}</h3>
+          <h3 className="card-title text-lg text-gray-300">{title}</h3>
           <p className="text-gray-400 text-sm">{dates}</p>
         </div>
-        <ArrowUpRightIcon className="w-5 h-5 text-gray-400" />
+        <ArrowUpRightIcon className="w-5 h-5 text-gray-400 transform transition-transform duration-300" />
       </div>
       <div className="flex mt-3">
-        {badges.map((badge, index) => (
-          <span key={index} className="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline bg-neutral-800 text-gray-200 rounded mr-2">{badge}</span>
+        {skills.map((skill, index) => (
+          <span
+            key={index}
+            className="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline bg-neutral-800 text-gray-200 rounded mr-2"
+          >
+            {skill}
+          </span>
         ))}
       </div>
     </div>
@@ -21,3 +26,4 @@ const Card = ({ title, dates, badges }) => {
 };
 
 export default Card;
+
--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -1,11 +1,23 @@
 "use client";
+import Link from "next/link";
+import Card from "../components/Card.jsx";
 
-function Page() {
+import { map } from "./mapping.js";
+
+function WorkPage() {
   return (
     <div>
-      
+      {map.map((item) => (
+        <Link href={`/work/${item.id}`} key={item.id}>
+          <Card
+            title={item.project_title}
+            dates={item.dates}
+            skills={item.skills}
+          />
+        </Link>
+      ))}
     </div>
   );
 }
 
-export default Page;
+export default WorkPage;
