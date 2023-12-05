92e337f4198b2d5b085b23988a924565163a3ca9
Camille Loue <camille.loue28@gmail.com>
2023-12-05 23:12:13 +0000

    style: color change

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -7,53 +7,63 @@ function Header() {
   const pathname = usePathname();
 
   return (
-    <div className="h-54 py-10 px-10 lg:px-0">
-      <ul className="flex gap-x-6">
-        <li>
-          <Link
-            className={classNames(
-              "text-neutral-500 hover:text-white",
-              pathname === "/" ? "text-white" : ""
-            )}
-            href="/"
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={classNames(
-              "text-neutral-500 hover:text-white",
-              pathname === "/work" ? "text-white" : ""
-            )}
-            href="/work"
-          >
-            Work
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={classNames(
-              "text-neutral-500 hover:text-white",
-              pathname === "/projects" ? "text-white" : ""
-            )}
-            href="/projects"
-          >
-            Projects
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={classNames(
-              "text-neutral-500 hover:text-white",
-              pathname === "/contact" ? "text-white" : ""
-            )}
-            href="/contact"
-          >
-            Contact
-          </Link>
-        </li>
-      </ul>
+    <div className="flex justify-between mt-2">
+      <div className="flex flex-col py-8">
+        <Link href="/" className="text-base text-zinc-300 hover:text-white">
+          Camille Loue
+        </Link>
+        <Link href="/" className="text-sm text-zinc-500">
+          ML Engineer & Indie Maker
+        </Link>
+      </div>
+      <div className="py-8 px-9 lg:px-0">
+        <ul className="flex gap-x-6">
+          <li>
+            <Link
+              className={classNames(
+                "text-zinc-500 hover:text-white",
+                pathname === "/" ? "text-white" : ""
+              )}
+              href="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={classNames(
+                "text-zinc-500 hover:text-white",
+                pathname === "/work" ? "text-white" : ""
+              )}
+              href="/work"
+            >
+              Work
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={classNames(
+                "text-zinc-500 hover:text-white",
+                pathname === "/projects" ? "text-white" : ""
+              )}
+              href="/projects"
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={classNames(
+                "text-zinc-500 hover:text-white",
+                pathname === "/contact" ? "text-white" : ""
+              )}
+              href="/contact"
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
