65831f2d4ef820e6941a877534237cd4279e9a27
Camille Loue <camille.loue28@gmail.com>
2023-12-05 23:13:16 +0000

    style: color change

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -3,7 +3,7 @@ import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
 
 const Card = ({ title, dates, skills }) => {
   return (
-    <div className="border font-sans border-neutral-800 bg-neutral-900 rounded flex flex-col justify-between mb-5 px-3 py-3 w-full cursor-pointer hover:card-hover">
+    <div className="border font-sans border-zinc-800 bg-zinc-900 rounded flex flex-col justify-between mb-5 px-3 py-3 w-full cursor-pointer hover:card-hover">
       <div className="flex justify-between items-center">
         <div>
           <h3 className="card-title text-lg text-gray-300">{title}</h3>
@@ -15,7 +15,7 @@ const Card = ({ title, dates, skills }) => {
         {skills.map((skill, index) => (
           <span
             key={index}
-            className="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline bg-neutral-800 text-gray-200 rounded mr-2"
+            className="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline bg-zinc-800 text-gray-200 rounded mr-2"
           >
             {skill}
           </span>
