a3b54eb79fddfe2c852f555abc9128ec4425aaec
Camille Loue <camille.loue28@gmail.com>
2023-12-07 17:34:39 +0000

    add product hunt logo to home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,16 +12,16 @@ export default function Home() {
           height={120}
         />
         <div>
-          <h1 className="mt-6 text-xl text-zinc-200">Hi, I&apos;m Camille!</h1>
+          <h1 className="mt-6 text-xl text-zinc-200">hi, I&apos;m Camille</h1>
           <div>
-            <p className="mt-2 text-zinc-400">
+            <p className="mt-2 text-lg text-zinc-400">
               I launch ML models and web apps.
             </p>
           </div>
         </div>
       </div>
 
-      <div className="w-3/4 text-base text-zinc-400 mt-12">
+      <div className="w-3/4 font-sans text-base text-zinc-400 mt-12">
         For the past two years, I&apos;ve worked as a freelance ML engineer and
         have been building web apps alongside. While machine learning is a key
         part of my journey, creating web apps is where I have the most fun. Feel
@@ -88,6 +88,19 @@ export default function Home() {
             <Image
               src="/images/social_media/linkedin.svg"
               alt="LinkedIn Logo"
+              width={28}
+              height={28}
+              className="h-5 w-5"
+            />
+          </a>
+          <a
+            href="https://www.producthunt.com/@camille_loue"
+            target="_blank"
+            className="opacity-70 hover:opacity-100"
+          >
+            <Image
+              src="/images/social_media/product-hunt.svg"
+              alt="Product Hunt Logo"
               width={28}
               height={28}
               className="h-5 w-5"
