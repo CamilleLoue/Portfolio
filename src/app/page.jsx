d0057baa2650131e8d7530b152f59893c92ad0f7
Camille Loue <camille.loue28@gmail.com>
2023-12-11 16:30:53 +0000

    style: color adjustments on home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="flex lg:flex-row gap-6 items-center">
         <div className="rounded-full w-24 h-24 lg:w-32 lg:h-32 aspect-square">
           <Image
-          className="rounded-full"
+            className="rounded-full"
             src="/images/profile.png"
             alt="Profile Picture"
             layout="responsive"
@@ -19,100 +19,102 @@ export default function Home() {
             hi, I&apos;m Camille
           </h1>
           <div>
-            <p className="w-5/6 lg:w-full lg:mt-2 text-base lg:text-lg text-zinc-400">
+            <p className="w-5/6 lg:w-full lg:mt-2 text-base font-light lg:text-lg text-zinc-400">
               I launch ML models and web apps.
             </p>
           </div>
         </div>
       </div>
 
-      <div className="lg:w-3/4 w-7/8 font-sans text-base text-zinc-400 mt-6 lg:mt-12">
+      <div className="lg:w-3/4 w-7/8 font-sans text-base text-opacity-90 text-zinc-300 mt-8 lg:mt-12">
         For the past two years, I&apos;ve worked as a freelance ML engineer and
         have been building web apps alongside. While machine learning is a key
         part of my journey, creating web apps is where I have the most fun. Feel
         free to have a look at my{" "}
         <a
           href="/work"
-          className="text-zinc-300 underline underline-offset-2 decoration-zinc-300/50 hover:text-white"
+          className="text-zinc-100 underline underline-offset-2 decoration-zinc-200/50 hover:text-white hover:decoration-zinc-200"
         >
           ML work
         </a>
         , and my{" "}
         <a
           href="/projects"
-          className="text-zinc-300 underline underline-offset-2 decoration-zinc-300/50 hover:text-white"
+          className="text-zinc-100 underline underline-offset-2 decoration-zinc-200/50 hover:text-white hover:decoration-zinc-200"
         >
           projects
         </a>
         .
       </div>
 
-      <section className="mt-8 lg:mt-12 text-base text-zinc-400">
+      <section className="mt-8 lg:mt-12 text-base text-opacity-90 text-zinc-300">
         If you have anything in mind,{" "}
         <a
           href="mailto:contact@camilleloue.com"
-          className="text-zinc-300 underline underline-offset-2 decoration-zinc-300/50 hover:text-white"
+          className="text-zinc-100 underline underline-offset-2 decoration-zinc-300/50 hover:text-white hover:decoration-zinc-200"
         >
           contact me
         </a>
         .
-        <div className="flex items-center gap-3 mt-8 lg:mt-12">
-          <h2 className="text-base text-zinc-400">
+        <div className="flex flex-col lg:flex-row gap-3 mt-8 lg:mt-12">
+          <h2 className="text-base text-zinc-300 text-opacity-90 order-1 lg:order-none">
             You can also find me on
           </h2>
 
-          <a
-            href="https://github.com/CamilleLoue"
-            target="_blank"
-            className="opacity-70 hover:opacity-100"
-          >
-            <Image
-              src="/images/social_media/github.svg"
-              alt="Github Logo"
-              width={28}
-              height={28}
-              className="h-5 w-5"
-            />
-          </a>
-          <a
-            href="https://twitter.com/cam_loue"
-            target="_blank"
-            className="opacity-70 hover:opacity-100"
-          >
-            <Image
-              src="/images/social_media/X.svg"
-              alt="X Logo"
-              width={28}
-              height={28}
-              className="h-5 w-5"
-            />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/camille-loue"
-            target="_blank"
-            className="opacity-70 hover:opacity-100"
-          >
-            <Image
-              src="/images/social_media/linkedin.svg"
-              alt="LinkedIn Logo"
-              width={28}
-              height={28}
-              className="h-5 w-5"
-            />
-          </a>
-          <a
-            href="https://www.producthunt.com/@camille_loue"
-            target="_blank"
-            className="opacity-70 hover:opacity-100"
-          >
-            <Image
-              src="/images/social_media/product-hunt.svg"
-              alt="Product Hunt Logo"
-              width={28}
-              height={28}
-              className="h-5 w-5"
-            />
-          </a>
+          <div className="flex items-center order-2 lg:order-none">
+            <a
+              href="https://github.com/CamilleLoue"
+              target="_blank"
+              className="mr-3 opacity-80 hover:opacity-100"
+            >
+              <Image
+                src="/images/social_media/github.svg"
+                alt="Github Logo"
+                width={28}
+                height={28}
+                className="h-5 w-5"
+              />
+            </a>
+            <a
+              href="https://twitter.com/cam_loue"
+              target="_blank"
+              className="mr-3 opacity-80 hover:opacity-100"
+            >
+              <Image
+                src="/images/social_media/X.svg"
+                alt="X Logo"
+                width={28}
+                height={28}
+                className="h-5 w-5"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/camille-loue"
+              target="_blank"
+              className="mr-3 opacity-80 hover:opacity-100"
+            >
+              <Image
+                src="/images/social_media/linkedin.svg"
+                alt="LinkedIn Logo"
+                width={28}
+                height={28}
+                className="h-5 w-5"
+              />
+            </a>
+            <a
+              href="https://www.producthunt.com/@camille_loue"
+              target="_blank"
+              className="opacity-80 hover:opacity-100"
+            >
+              <Image
+                src="/images/social_media/product-hunt.svg"
+                alt="Product Hunt Logo"
+                width={28}
+                height={28}
+                className="h-5 w-5"
+              />
+            </a>
+          </div>
         </div>
       </section>
     </main>
